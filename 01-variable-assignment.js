919c832755b540dc7278e42e7fe6146efa1c6e69
dipalisangekar <dipalisangekar@gmail.com>
2024-08-01 00:31:54 +0530

    JavaScript variables
    JavaScript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,7 @@
+
+console.log("1.My city name is : Solapur");
+console.log("2.My Best friend name is : Ashwini");
+console.log("3.My Country name is : India");
+console.log("4.My School name is : SMVVP");
+console.log("5.My Collage name is : BMIT");
+
