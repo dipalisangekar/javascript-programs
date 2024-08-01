db45de4e2edfd512e668e90262cec54af048d63c
dipalisangekar <dipalisangekar@gmail.com>
2024-08-01 23:02:50 +0530

    Added Assign of typeof
    Added Assign of typeof

---
+++ 02-typeOf.js
@@ -0,0 +1,36 @@
+console.log("=======================STEP1===================");
+var bankName;
+console.log("Value before initialization is:",bankName);
+var bankName="SBI Bank";
+console.log("Value after initialization is:",bankName)
+console.log("Bank Name is:",bankName);
+var bankNameTypeOf = typeof bankName;
+console.log("Datatype of BankName is:",bankNameTypeOf);
+
+console.log("=======================STEP2===================");
+var mark = '90%';
+console.log("Mark is:",mark);
+var markTypeOf = typeof mark;
+console.log("Datatype of mark is:",markTypeOf);
+
+console.log("=======================STEP3===================");
+var isWorking;
+console.log("value before Initialization is:",isWorking);
+console.log("Data type of isWorking is:",isWorking);
+var isWorking = true;
+console.log("value After intialization is:",isWorking);
+var isWorkingTypeOf = typeof isWorking;
+console.log("Data type of isWorking after initialization is:",isWorkingTypeOf);
+
+console.log("=======================STEP4===================");
+var totalCount = "Hundred and Seven";
+console.log("Total count is:",totalCount);
+var totalCountTypeOf = typeof totalCount;
+console.log("a.Datatype of totalCount is:",totalCountTypeOf);
+
+var totalCount = 107;
+console.log("Total count is:",totalCount);
+var totalCountTypeOf = typeof totalCount;
+console.log("b.Datatype of totalCount is:",totalCountTypeOf);
+
+
