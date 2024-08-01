76abb1c06d474d1e9e3270681f472386a3882aac
dipalisangekar <dipalisangekar@gmail.com>
2024-08-01 21:55:13 +0530

    Added variable assignment
    Added variable assignment

---
+++ 01-variableAssign.js
@@ -0,0 +1,47 @@
+console.log("----------------STEP 1---------------");
+var myFullName;
+console.log("1.My Full Name is:",myFullName);
+
+var myFullName = "Dipali Sangekar";
+console.log("2.My First and Last Name is:",myFullName);
+
+myFullName = "Dipali Nagendra sangekar";
+console.log("3.My FullName is:",myFullName);
+
+console.log("----------------STEP 2---------------");
+var myLove="I love only JS";
+console.log("1.My Love is:",myLove);
+
+console.log("----------------STEP 3---------------");
+var firstName = "Dipali";
+console.log("1.My First name is:",firstName);
+
+var lastName = "Sangekar";
+console.log("2.My Last name is:",lastName);
+
+var age = "33";
+console.log("3.My Age is:",age);
+
+var address = "Pune";
+console.log("4.My Address is:",address);
+
+var pinCode = "410057";
+console.log("5.My PinCode is:",pinCode);
+
+var gender = "Female";
+console.log("6.My Gender is:",gender);
+
+var isMarried = "True";
+console.log("7.My Marital status is:",isMarried);
+
+
+
+
+
+
+
+
+
+
+
+
