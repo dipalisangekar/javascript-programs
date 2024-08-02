70d55918ab4a2872895a228bdd23ab82d7480da6
dipalisangekar <dipalisangekar@gmail.com>
2024-08-02 13:34:05 +0530

    Added Function Assign 02
    Added Function Assign 02

---
+++ 03-funAssign02.js
@@ -0,0 +1,17 @@
+
+console.log("====================Normal Function===================");
+ function bankDetails(bankName,accountNum,location,pinCode){
+ 
+    console.log("Bank details are:");
+    console.log("Bank Name is:",bankName);
+    console.log("Account number is:",accountNum);
+    console.log("Location is:",location);
+    console.log("Pincode is:",pinCode);
+    
+   }
+    console.log("=================1.2.1============");
+    bankDetails("CITI Bank",3456782345,"Pune",431202);
+    console.log("=================1.2.2============");
+    bankDetails("Axis Bank",7856782345,"Mumbai",441202);
+    console.log("=================1.2.3============");
+    bankDetails("HDFC Bank",8956782345,"Pune",631202,"Open");
