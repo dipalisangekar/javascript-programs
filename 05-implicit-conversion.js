2db390366a7dc3243d54214ca3a6b2978010c0ca
dipalisangekar <dipalisangekar@gmail.com>
2024-08-09 23:34:24 +0530

    added implicit conversion
    added implicit conversion

---
+++ 05-implicit-conversion.js
@@ -0,0 +1,19 @@
+console.log('================Implicit conversion to string====================');
+
+console.log(`Numeric string used with +gives string type`);
+   let result;
+
+   result ='3' + 2;
+   console.log(result);
+
+   result = '3' + true;
+   console.log(result);
+
+   result = '3' + undefined;
+   console.log(result);
+
+   result = '3' + null;
+   console.log(result);
+
+
+
