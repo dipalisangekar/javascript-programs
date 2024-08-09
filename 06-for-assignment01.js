7a228c519c7868266d6761f7c955ee405d75b29b
dipalisangekar <dipalisangekar@gmail.com>
2024-08-09 23:35:00 +0530

    Added for-loop assign1
    Added for-loop assign1

---
+++ 06-for-assignment01.js
@@ -0,0 +1,35 @@
+
+console.log('============== 1. 5 to 15 by incrementing 1=======================');
+ for (let i = 5; i<=15 ; i=i+1) {
+    console.log(i);
+}
+
+console.log('==============2.50 to 40 by decrementing 1=======================');
+ for (let i = 50; i>=40; i=i-1) {
+    console.log(i);
+   }
+    
+console.log('==============3. First 15 odd numbers=======================');
+  for (let i = 1; i<=30 ; i=i+2) {
+        console.log(i);
+   }
+        
+console.log('==============4. First 10 Even numbers=======================');
+   for (let i = 2; i<=20 ; i=i+2) {
+        console.log(i);
+    }
+            
+console.log('==============5. Table of 5=======================');
+    for (let i = 5; i <=50 ; i=i+5) {
+        console.log(i);
+    }
+
+console.log('==============6. Table of 10============');
+    for (let i = 10; i <=100 ; i=i+10) {
+        console.log(i);
+    }
+                    
+console.log('==============7. Table of 10 reverse order=======================');
+    for (let i = 100; i >=10; i=i-10) {
+        console.log(i);
+    }
